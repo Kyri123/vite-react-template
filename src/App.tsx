307316524b96ecd92e124cf2af54a33806e9f103
Oliver <olli143@live.de>
2023-04-03 06:20:39 +0200

    eslint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [ Count, setCount ] = useState( { count: 0 } );
 
-  return (
-    <div className="App">
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	return (
+		<div className="App">
+			<div></div>
+			<h1>Vite + React</h1>
+			<div className="card">
+				<button
+					onClick={ () =>
+						setCount( count => ( {
+							count: count.count + 1
+						} ) )
+					}>
+					count is { Count.count }
+				</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,32 @@
-import { defineConfig } from 'vite'
-import react            from '@vitejs/plugin-react'
-import eslint           from 'vite-plugin-eslint';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
 
 export default defineConfig({
-  /*server: {
-    proxy: {
-      "/api": {
-        target: 'http://85.214.47.99:26080',
-        changeOrigin: true,
-        secure: false,
-        ws: true
-      }
-    }
-  },*/
-  build: {
-    outDir: "build"
-  },
-  plugins: [react({
-    include: "**/*.tsx",
-    babel: {
-      parserOpts: {
-        plugins: ['decorators-legacy'],
-      },
-    },
-  }), eslint( {
-    include: "**/*.tsx"
-  } )]
-})
+	/*server: {
+	 proxy: {
+	 "/api": {
+	 target: 'http://85.214.47.99:26080',
+	 changeOrigin: true, 
+	 secure: false,
+	 ws: true
+	 }
+	 }
+	 },*/
+	build: {
+		outDir: "build"
+	},
+	plugins: [
+		react({
+			include: "**/*.tsx",
+			babel: {
+				parserOpts: {
+					plugins: ["decorators-legacy"]
+				}
+			}
+		}),
+		eslint({
+			include: "**/*.tsx"
+		})
+	]
+});
